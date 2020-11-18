2a9ccf7e74ce22ffd4e16e08b92eba7c1180259b
evgeny knyazkin <evgen.kudesnik@gmail.com>
2020-11-18 22:47:31 +0300

    1 number

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "49164432001949655";
+        String number = "1";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
